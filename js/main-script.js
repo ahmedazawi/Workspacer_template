e6fc3a95b82b1092243e964e9832b306a6a8d718
Ahmed husam <ahmedhusam@gmail.com>
2021-08-17 11:40:20 +0300

    UPDATE

--- js/main-script.js
+++ js/main-script.js
@@ -15,28 +15,28 @@ for (let i = 0; i < elem.length; i++) {
 
 // Sidebar animation
 
-if (window.innerWidth <= 1100) {
+// if (window.innerWidth <= 1100) {
 
-    window.onscroll = function () { scrollFunction() };
+//     window.onscroll = function () { scrollFunction() };
 
-    function scrollFunction() {
-        if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-            document.querySelector(".sidebar-items").style.padding = "30px 0";
-            // document.querySelector(".sidebar .sidebar-menu ").style.fontSize = "25px";
-            document.querySelector(".sidebar").style.padding = "30px 0";
+//     function scrollFunction() {
+//         if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+//             document.querySelector(".sidebar-items").style.padding = "30px 0";
+//             document.querySelector(".sidebar .sidebar-menu ").style.fontSize = "25px";
+//             document.querySelector(".sidebar").style.padding = "30px 0";
 
-        } else {
-            document.querySelector(".sidebar-items").style.padding = "80px 0";
-            document.querySelector(".sidebar").style.padding = "80px 0";
+//         } else {
+//             document.querySelector(".sidebar-items").style.padding = "80px 0";
+//             document.querySelector(".sidebar").style.padding = "80px 0";
 
-            // document.querySelector(".sidebar .sidebar-menu ").style.fontSize = "15px";
-        }
-    }
-
-
+//             document.querySelector(".sidebar .sidebar-menu ").style.fontSize = "15px";
+//         }
+//     }
 
 
-}
+
+
+// }
 
 
 
